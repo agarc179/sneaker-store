41df20f72c972945d13ab2e2de49ecaa73223c2d
Adrian Garcia <tej18359@gmail.com>
2018-10-24 15:54:12 -0500

    comment out SQL code
    comment out SQL code due to it was breaking my milestone 3 code

--- src/main/java/com/sneaker/sneakerstore/sneaker/sneakerShop/spring/Main.java
+++ src/main/java/com/sneaker/sneakerstore/sneaker/sneakerShop/spring/Main.java
@@ -1,46 +1,48 @@
-package com.sneaker.sneakerstore.sneaker.sneakerShop.spring;
-
-import com.sneaker.sneakerstore.sneaker.sneakerShop.jpa.Sneaker;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@SpringBootApplication
-@EnableJpaAuditing
-@ComponentScan("com.sneaker.sneakerstore.sneaker")
-public class Main implements CommandLineRunner {
-
-    @Autowired
-    SneakerRepository repository;
-
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Main.class);
-        app.run(args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        createExample();
-
-    }
-
-    private void createExample() {
-        System.out.println(repository.count());
-        Sneaker sneaker = new Sneaker();
-        sneaker.setId("Air209445");
-        sneaker.setName("Air Force One");
-        sneaker.setBrand("Nike");
-        sneaker.setColor("White");
-        sneaker.setSize("11");
-        sneaker.setType("Casual");
-        sneaker.setPrice("90");
-        sneaker.setGender("male");
-
-        repository.save(sneaker);
-
-        System.out.println(repository.count());
-    }
-}
+//package com.sneaker.sneakerstore.sneaker.sneakerShop.spring;
+//
+//import com.sneaker.sneakerstore.sneaker.sneakerShop.jpa.Sneaker;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//
+//@SpringBootApplication
+//@EnableJpaAuditing
+//@EnableJpaRepositories
+////@ComponentScan("com.sneaker.sneakerstore.sneaker")
+//public class Main implements CommandLineRunner {
+//
+//    @Autowired
+//    SneakerRepository repository;
+//
+//    public static void main(String[] args) {
+//        SpringApplication app = new SpringApplication(Main.class);
+//        app.run(args);
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        createExample();
+//
+//    }
+//
+//    private void createExample() {
+//        System.out.println(repository.count());
+//        Sneaker sneaker = new Sneaker();
+//        sneaker.setId("Air209445");
+//        sneaker.setName("Air Force One");
+//        sneaker.setBrand("Nike");
+//        sneaker.setColor("White");
+//        sneaker.setSize("11");
+//        sneaker.setType("Casual");
+//        sneaker.setPrice("90");
+//        sneaker.setGender("male");
+//
+//        repository.save(sneaker);
+//
+//        System.out.println(repository.count());
+//    }
+//}
