c0f290d496f641362c9a45dfd24302f73ddee617
Adrian Garcia <tej18359@gmail.com>
2018-11-01 16:33:42 -0500

    post method to mongo db
    implemented the post call to mondoDB that querys the database by the zipcode and it returns store name and zipcode of the store

--- src/main/java/com/sneaker/sneakerstore/sneaker/sneakerShop/controllers/SneakerShopController.java
+++ src/main/java/com/sneaker/sneakerstore/sneaker/sneakerShop/controllers/SneakerShopController.java
@@ -1,13 +1,17 @@
 package com.sneaker.sneakerstore.sneaker.sneakerShop.controllers;
 
 import com.sneaker.sneakerstore.sneaker.sneakerShop.RaffleInfo;
-import com.sneaker.sneakerstore.sneaker.sneakerShop.StoreLookUp;
+import com.sneaker.sneakerstore.sneaker.sneakerShop.entities.Store;
+import com.sneaker.sneakerstore.sneaker.sneakerShop.interfaces.IStore;
+import com.sneaker.sneakerstore.sneaker.sneakerShop.services.StoreService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class SneakerShopController {
@@ -34,12 +38,20 @@ public class SneakerShopController {
 
     @GetMapping("/storelookup")
     public String storeLookUpForm(Model model){
-        model.addAttribute("storelookup", new StoreLookUp());
+        model.addAttribute("storelookup", new Store());
         return "storelookup";
     }
 
     @PostMapping("/storelookup")
-    public String storeLookUpSubmit(@ModelAttribute StoreLookUp storeLookUp){
+    public String storeLookUpSubmit(IStore store, Model model){
+        StoreService storeService = new StoreService();
+        List<IStore> stores = storeService.getStore(store.getZip());
+
+        for(IStore s: stores){
+            model.addAttribute("storelookup", s);
+        }
+
+        //model.addAttribute("storelookup", store);
         return "storelookup";
     }
 }
